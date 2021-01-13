9c96c6271629934d945328fa7813da39f1fbc31b
Stuart Hicks <stuart.hicks@gmail.com>
2021-01-13 15:06:06 +0100

    Adding the various Game Components to the main file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Sky } from 'drei';
+import React from 'react'
+import { Canvas } from 'react-three-fiber';
+import { Vector3 } from 'three';
+import { Physics } from 'use-cannon';
+import Camera from './gameComponents/Camera'
+import Ground from './gameComponents/Ground'
+import Player from './gameComponents/Player'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Canvas shadowMap sRGB gl={{ alpha: false}}>
+      <Camera />
+      <Sky sunPosition={new Vector3(100, 10, 100)} />
+      <ambientLight intensity={0.3} />
+      <pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
+      <Physics gravity={[0, -30, 0]}>
+        <Ground />
+        <Player />
+      </Physics>
+    </Canvas>
+  )
 }
 
 export default App;
