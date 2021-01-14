60d8f40bdb593231e0e054806ec62f124d6f37e5
shicks1080 <stuart.hicks@gmail.com>
2021-01-14 13:24:05 +0100

    Giving the player the player the ability to move and jump
    We create a custom hook to manage the state of the player's movement. Then pop that into the Player component.

--- src/gameComponents/Player.js
+++ src/gameComponents/Player.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 import { useFrame, useThree } from 'react-three-fiber'
+import { Vector3 } from 'three';
 import { useSphere } from 'use-cannon'
 
+import usePlayerControls from '../gameHooks/usePlayerControls'
 import CustomPointerLockControls from './CustomPointerLockControls'
+
+const SPEED = 5
 
 const Player = (props) => {
     const { camera } = useThree()
+
+    const {
+        moveForward,
+        moveBackward,
+        moveLeft,
+        moveRight,
+        jump
+    } = usePlayerControls()
 
     const [ref, api] = useSphere(() => ({
         mass: 1,
@@ -14,8 +26,36 @@ const Player = (props) => {
         ...props
     }))
 
+    // We can store any sort of value here, like a class's property field
+    const velocity = useRef([0, 0, 0])
+    useEffect(() => {
+        api.velocity.subscribe(v => velocity.current = v)
+    }, [])
+
+    // This is where we update the physics for the Player
     useFrame(() => {
         camera.position.copy(ref.current.position)
+
+        // We align the direction of the Player with the Camera direction
+        const direction = new Vector3()
+
+        // This allows simultaneous button pressing
+        const frontVector = new Vector3(0, 0, Number(moveBackward) - Number(moveForward))
+        const sideVector = new Vector3(Number(moveLeft) - Number(moveRight), 0, 0)
+
+        direction
+            .subVectors(frontVector, sideVector)
+            .normalize()
+            .multiplyScalar(SPEED)
+            .applyEuler(camera.rotation)
+        
+        api.velocity.set(direction.x, velocity.current[1], direction.z)
+
+        // Velocity will satisfy this when the Player's on the ground
+        // and when they're at the crest of a jump allowing a double jump! 
+        if(jump && Math.abs(velocity.current[1].toFixed(2)) < 0.05) {
+            api.velocity.set(velocity.current[0], 10, velocity.current[2])
+        }
     })
 
     return (
---
+++ src/gameHooks/usePlayerControls.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react"
+
+function moveFieldByKey(key) {
+  const keys = {
+    KeyW: "moveForward",
+    KeyS: "moveBackward",
+    KeyA: "moveLeft",
+    KeyD: "moveRight",
+    Space: "jump"
+  }
+  return keys[key]
+}
+
+const usePlayerControls = () => {
+  const [ movement, setMovement ] = useState({
+    moveForward: false,
+    moveBackward: false,
+    moveLeft: false,
+    moveRight: false,
+    jump: false
+  })
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      setMovement(m => ({
+        ...m,
+        [moveFieldByKey(e.code)]: true
+      }))
+    }
+
+    const handleKeyUp = (e) => {
+      setMovement(m => ({
+        ...m,
+        [moveFieldByKey(e.code)]: false
+      }))
+    }
+
+    document.addEventListener("keydown", handleKeyDown)
+    document.addEventListener("keyup", handleKeyUp)
+
+    // This is called when we unmount the hook
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown)
+      document.removeEventListener("keyup", handleKeyUp)
+    }
+  }, [])
+
+  return movement
+}
+
+export default usePlayerControls
